12e86bacb835081f4a3e6677d5861bd62eb50d70
Jacqui <jacqui@broadstone-engage.com>
2019-10-17 21:56:52 +0100

    ~ library styles

--- projects/cornerstone-library/src/lib/cornerstone-library.component.ts
+++ projects/cornerstone-library/src/lib/cornerstone-library.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
       cornerstone-library works!
     </p>
   `,
-  styleUrls: ["./index.scss"]
+  styleUrls: ["./cornerstone-library.component.scss"]
 })
 export class CornerstoneLibraryComponent implements OnInit {
 
